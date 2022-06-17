fb5d30fb3b3a83098522f9af6e5d9db3423a5e51
pruizf <pabloruizfabo@gmail.com>
2022-06-17 18:56:17 +0200

    tidies up previous commit

--- scripts/add_emo_pol_to_rhymes.py
+++ scripts/add_emo_pol_to_rhymes.py
@@ -28,9 +28,10 @@ brutality	brutalidad	fear	0.922
 # add vad columns
 # add columns for emotion intensity
 # iterate over corpus df
-#   for each lemma
-#     iterate over vad and EI to collect infos
-#     add them to the df
+# hash the lexica
+# for each word-form
+#   add infos to df if found in lexica
+#   if not found, see if find infos for their lemma and add
 
 
 def collect_emotions_per_term(lx):
@@ -61,6 +62,10 @@ def collect_vad_per_term(lx):
 
 
 def add_annots_to_df(df, idx, annots, target):
+  """
+  Given a dataframe at index `idx`, add its emotion and VAD
+  annotations to the relevant (emotion_call or emotion_echo) column
+  """
   assert target in ("call", "echo")
   for ename, escore in annots.items():
     df.loc[idx, f"{ename}_{target}"] = escore
@@ -84,24 +89,21 @@ if __name__ == "__main__":
   for vadname in cf.vadnames:
     cdf[f"{vadname}_call"] = np.nan
     cdf[f"{vadname}_echo"] = np.nan
+  print(f"-   Populate df [{strftime('%R')}]")
   #TODO: actually, should look at the word-form too if lemma not found in lexica
   for idx, row in cdf.iterrows():
     call_lemma = row.CallLemma
     echo_lemma = row.EchoLemma
     #if lemma == "gloria":
     #  breakpoint()
+    # get emo and vad for row
     einfos_call = lem2emo.get(call_lemma)
     einfos_echo = lem2emo.get(echo_lemma)
     vinfos_call = lem2vad.get(call_lemma)
     vinfos_echo = lem2vad.get(echo_lemma)
     vinfos_call_raw = lem2vad_raw.get(call_lemma)
     vinfos_echo_raw = lem2vad_raw.get(echo_lemma)
-    # if einfos_call is not None:
-    #   for ename, escore in einfos_call.items():
-    #     cdf.loc[idx, f"{ename}_call"] = escore
-    # if einfos_echo is not None:
-    #   for ename, escore in einfos_echo.items():
-    #     cdf.loc[idx, f"{ename}_echo"] = escore
+    # populate emo and vad columns
     if einfos_call is not None:
       add_annots_to_df(cdf, idx, einfos_call, "call")
     if einfos_echo is not None:
@@ -110,5 +112,8 @@ if __name__ == "__main__":
       add_annots_to_df(cdf, idx, vinfos_call, "call")
     if vinfos_echo is not None:
       add_annots_to_df(cdf, idx, vinfos_echo, "echo")
+    if idx > 0 and not idx % 5000:
+      print(f"- Done echos {idx} poems [{strftime('%R')}]")
+
   cdf.to_csv(cf.df_emos, sep='\t', index=False)
   print(f"- End [{strftime('%R')}]")
