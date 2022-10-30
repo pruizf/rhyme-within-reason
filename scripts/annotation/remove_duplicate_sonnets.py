bd571647dc15d67bfe6000a922083d6b3aa47b85
pruizf <pabloruizfabo@gmail.com>
2022-10-30 14:26:35 +0100

    [dt] [sc] dup sonnet IDs removed from signature files

--- scripts/annotation/remove_duplicate_sonnets.py
+++ scripts/annotation/remove_duplicate_sonnets.py
@@ -16,30 +16,50 @@ orig_emo_df_fn = os.path.join(datadir, "dataframe-all_lem_sets_emos_with_nrc.tsv
 orig_emo_df_fn_without_nrc = os.path.join(datadir, "dataframe-all_lem_sets_emos.tsv")
 orig_highemo_df_fn = os.path.join(datadir, "highemos_06_counts_per_poem_with_nrc.tsv")
 orig_highemo_df_fn_without_nrc = os.path.join(datadir, "highemos_06_counts_per_poem.tsv")
+# Note: naming logic is the opposite in the data and in this script (with/without nrc)
+signatures_fn = os.path.join(datadir, "signatures_with_nrc.tsv")
+signatures_fn_without_nrc = os.path.join(datadir, "signatures.tsv")
 
 with open(ids_to_keep_fn, mode="r", encoding="utf8") as idfh:
   ids_to_keep = set([ll.strip() for ll in idfh])
 
+# original dataframes
 orig_emo_df = pd.read_csv(orig_emo_df_fn, sep="\t")
 orig_highemo_df = pd.read_csv(orig_highemo_df_fn, sep="\t")
 orig_emo_df_without_nrc = pd.read_csv(orig_emo_df_fn_without_nrc, sep="\t")
 orig_highemo_df_without_nrc = pd.read_csv(orig_highemo_df_fn_without_nrc, sep="\t")
+signatures_df = pd.read_csv(signatures_fn, sep="\t")
+signatures_df_without_nrc = pd.read_csv(signatures_fn_without_nrc, sep="\t")
 
+# filtered dataframes
 orig_emo_df_filtered = orig_emo_df.loc[orig_emo_df['SonnetID'].isin(ids_to_keep)]
 orig_highemo_df_filtered = orig_highemo_df.loc[orig_highemo_df['SonnetID'].isin(ids_to_keep)]
 orig_emo_df_filtered_without_nrc = \
   orig_emo_df_without_nrc.loc[orig_emo_df_without_nrc['SonnetID'].isin(ids_to_keep)]
 orig_highemo_df_filtered_without_nrc = \
   orig_highemo_df_without_nrc.loc[orig_highemo_df_without_nrc['SonnetID'].isin(ids_to_keep)]
+signatures_df_filtered = signatures_df.loc[signatures_df['SonnetID'].isin(ids_to_keep)]
+signatures_df_filtered_without_nrc = \
+  signatures_df_without_nrc.loc[signatures_df_without_nrc['SonnetID'].isin(ids_to_keep)]
 
+# write out
+#  all data
 orig_emo_df_filtered.to_csv(orig_emo_df_fn.replace(".tsv", "_filt.tsv"),
                             sep="\t", index=False)
 orig_highemo_df_filtered.to_csv(orig_highemo_df_fn.replace(".tsv", "_filt.tsv"),
                             sep="\t", index=False)
+signatures_df_filtered.to_csv(signatures_fn.replace(".tsv", "_filt.tsv"),
+                              sep="\t", index=False)
+#  no nrc
 orig_emo_df_filtered_without_nrc.to_csv(
   orig_emo_df_fn_without_nrc.replace(".tsv", "_filt.tsv"), sep="\t", index=False)
 orig_highemo_df_filtered_without_nrc.to_csv(
   orig_highemo_df_fn_without_nrc.replace(".tsv", "_filt.tsv"), sep="\t", index=False)
+signatures_df_filtered_without_nrc.to_csv(signatures_fn_without_nrc.replace(".tsv", "_filt.tsv"),
+                                          sep="\t", index=False)
 
-
+# log to console
 deleted_ids = set(orig_emo_df['SonnetID'].tolist()).difference(set(ids_to_keep))
+print("# Deleted IDs")
+for did in sorted(deleted_ids):
+  print(did)
